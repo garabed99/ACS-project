4de9437db4161c5f3f2945170505470a4aaff203
haigaz15 <43118062+haigaz15@users.noreply.github.com>
2021-05-18 16:46:10 +0400

    added temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 //import UsersTable from './UsersTable';
 import SignUp from './SignUp';
-//import SignIn from './SignIn';
-
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import SignIn from './SignIn';
 function App() {
   return (
-    <SignUp />
+    <Router>
+      <Switch>
+      <Route exact path= "/">
+        <SignUp/>
+        </Route>
+
+        <Route path= "/SignIn">
+          <SignIn />
+        </Route>
+        
+      </Switch>
+    </Router> 
   );
 }
+
 
 export default App;
